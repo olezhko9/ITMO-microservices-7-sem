852aa5e76d69a8fd38e1e62e6ce3872cdf07143f
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-10-19 04:48:10 +0300

    connected sqlite

--- warehouse-service/src/main/java/com/microservices/warehouse/dao/WarehouseDataService.java
+++ warehouse-service/src/main/java/com/microservices/warehouse/dao/WarehouseDataService.java
@@ -5,6 +5,7 @@ import com.microservices.warehouse.dto.ItemDto;
 import com.microservices.warehouse.model.Item;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,33 @@ import java.util.stream.Collectors;
 public class WarehouseDataService implements ItemDao {
 
     private static List<Item> DB = new ArrayList<>();
+    private static final String CONNECTION_STRING = "jdbc:sqlite:warehouse.db";
+    private Connection connection;
+
+    public WarehouseDataService() {
+        try {
+            connection = DriverManager.getConnection(CONNECTION_STRING);
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO items ('name', 'price', 'actualAmount', 'availableAmount')" +
+                            "VALUES ('Xiaomi Redmo Note 7', '13990.0', 56, 56)"
+            );
+            statement.executeUpdate();
+            ResultSet itemID = statement.getGeneratedKeys();
+            if (itemID.next()) {
+                System.out.format("Пользователь добавлен. id: %d%n",
+                        itemID.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert connection != null;
+                connection.close();
+            } catch (SQLException ignored) {}
+        }
+    }
+
 
     @Override
     public ItemDto addItem(UUID id, ItemCreationDto itemCreationDto) {
