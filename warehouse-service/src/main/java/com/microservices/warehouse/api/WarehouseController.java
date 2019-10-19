7b32b8a1eab62a36d9a51377177dc05cd12cb8ba
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-10-19 03:24:24 +0300

    updated api responses

--- warehouse-service/src/main/java/com/microservices/warehouse/api/WarehouseController.java
+++ warehouse-service/src/main/java/com/microservices/warehouse/api/WarehouseController.java
@@ -5,6 +5,7 @@ import com.microservices.warehouse.dto.ItemCreationDto;
 import com.microservices.warehouse.dto.ItemDto;
 import com.microservices.warehouse.service.WarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,24 +23,32 @@ public class WarehouseController {
     }
 
     @PostMapping(path = "/items")
-    public void addItem(@RequestBody ItemCreationDto item) {
-        warehouseService.addItem(item);
+    public ResponseEntity<ItemDto> addItem(@RequestBody ItemCreationDto item) {
+        return ResponseEntity.ok(warehouseService.addItem(item));
     }
 
     @GetMapping(path = "/items")
-    public List<ItemDto> getAllItems() {
-        return warehouseService.getAllItems();
+    public ResponseEntity<List<ItemDto>> getAllItems() {
+        return ResponseEntity.ok(warehouseService.getAllItems());
     }
 
     @GetMapping(path = "/items/{id}")
-    public ItemDto getItemById(@PathVariable("id") UUID id) {
-        return warehouseService.getItemById(id);
+    public ResponseEntity<ItemDto> getItemById(@PathVariable("id") UUID id) {
+        ItemDto itemDto = warehouseService.getItemById(id);
+        if (itemDto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(itemDto);
     }
 
     @PutMapping(path = "/items/{id}/amount")
-    public ItemDto updateItemAmount(@PathVariable("id") UUID id, @RequestBody ObjectNode json) {
+    public ResponseEntity<ItemDto> updateItemAmount(@PathVariable("id") UUID id, @RequestBody ObjectNode json) {
         String amountType = json.get("amountType").asText();
         int amount = json.get("amount").asInt();
-        return warehouseService.updateItemAmount(id, amountType, amount);
+        ItemDto itemDto = warehouseService.updateItemAmount(id, amountType, amount);
+        if (itemDto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(itemDto);
     }
 }
