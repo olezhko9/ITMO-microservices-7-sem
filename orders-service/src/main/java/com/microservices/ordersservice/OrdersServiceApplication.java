bb9950e3c0f213c036be0cc1fed7d866b97de3a7
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-12-21 03:46:03 +0300

    enable eureka client

--- orders-service/src/main/java/com/microservices/ordersservice/OrdersServiceApplication.java
+++ orders-service/src/main/java/com/microservices/ordersservice/OrdersServiceApplication.java
@@ -2,10 +2,11 @@ package com.microservices.ordersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrdersServiceApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +14,4 @@ public class OrdersServiceApplication {
 	}
 
 }
+
